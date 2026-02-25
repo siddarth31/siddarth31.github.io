7921b2013e8a5cff28870c1cea2633014ba31d9f
siddarth31 <siddarth31@users.noreply.github.com>
2026-02-25 03:18:10 +0000

    Deploying to gh-pages from @ siddarth31/siddarth31.github.io@ccca825cca7f627182239db1efb17b8ea0770ba9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-short-cv",
-          title: "short cv",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "nav-blog",
           title: "blog",
           description: "",
